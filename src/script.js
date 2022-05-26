987421ea3660f7e27a120e6654ab28a2b0d964c5
SkySourced <luka.gn.scott@gmail.com>
2022-05-27 09:35:01 +1200

    Score, death, gameState, more constants

--- src/script.js
+++ src/script.js
@@ -2,8 +2,13 @@ var WIDTH = 800;
 var HEIGHT = 800;
 var GRAVITY = 0.1;
 var SPEED_CAP = 10;
+var PLAYER_SIZE = 100;
+var PLATFORM_DISTANCE = 500;
+var SIDEBAR_WIDTH = 45;
 var ctx;
 var canvas;
+var score = 0;
+var gameState = "game";
 var LEFTSIDE = new Image();
 LEFTSIDE.src = "./assets/leftSide.png";
 var RIGHTSIDE = new Image();
@@ -16,6 +21,7 @@ var platformHeight = 100; // height of platforms
 var numPlatforms = 100;
 var platforms;
 var player;
+var collisionFlag; // used to check if player hits any platforms.
 var keysPressed = {
     left: false,
     right: false,
@@ -74,17 +80,17 @@ var Player = /** @class */ (function () {
         if (this.canDash && !this.dashing) {
             console.log("dashing");
             this.dashing = true;
-            if (this.facing == "right" && this.x + this.dashLength < WIDTH - 45) {
+            if (this.facing == "right" && this.x + this.dashLength < WIDTH - SIDEBAR_WIDTH) {
                 this.x += this.dashLength;
             }
-            else if (this.facing == "left" && this.x - this.dashLength > 45) {
+            else if (this.facing == "left" && this.x - this.dashLength > SIDEBAR_WIDTH) {
                 this.x -= this.dashLength;
             }
-            else if (this.facing == "right" && this.x + this.dashLength > WIDTH - 45) {
-                this.x == WIDTH - 45;
+            else if (this.facing == "right" && this.x + this.dashLength > WIDTH - SIDEBAR_WIDTH) {
+                this.x == WIDTH - SIDEBAR_WIDTH;
             }
-            else if (this.facing == "left" && this.x - this.dashLength < 45) {
-                this.x == 45;
+            else if (this.facing == "left" && this.x - this.dashLength < SIDEBAR_WIDTH) {
+                this.x == SIDEBAR_WIDTH;
             }
             this.dashing = false;
         }
@@ -96,28 +102,18 @@ var Player = /** @class */ (function () {
                 console.log("moving left");
                 this.facing = "left";
                 this.x -= this.moveSpeed;
-                for (var i = 0; i < platforms.length; i++) {
-                    if (this.x < platforms[i].x + platforms[i].width && this.x + this.width > platforms[i].x && this.y + this.height > platforms[i].y && this.y < platforms[i].y + platforms[i].height) {
-                        this.x += this.moveSpeed;
-                    }
-                }
             }
             else if (keysPressed.right) {
                 console.log("moving right");
                 this.facing = "right";
                 this.x += this.moveSpeed;
-                for (var i = 0; i < platforms.length; i++) {
-                    if (this.x < platforms[i].x + platforms[i].width && this.x + this.width > platforms[i].x && this.y + this.height > platforms[i].y && this.y < platforms[i].y + platforms[i].height) {
-                        this.x -= this.moveSpeed;
-                    }
-                }
             }
         }
-        if (this.x < 45) {
-            this.x = 45;
+        if (this.x < SIDEBAR_WIDTH) {
+            this.x = SIDEBAR_WIDTH;
         }
-        if (this.x > WIDTH - this.width - 45) {
-            this.x = WIDTH - this.width - 45;
+        if (this.x > WIDTH - this.width - SIDEBAR_WIDTH) {
+            this.x = WIDTH - this.width - SIDEBAR_WIDTH;
         }
     };
     return Player;
@@ -126,55 +122,64 @@ window.onload = function () {
     platforms = [];
     canvas = document.getElementById("mainCanvas");
     ctx = canvas.getContext("2d");
-    // Creates the platforms
-    for (var i = 0; i < numPlatforms; i++) {
-        platforms[platforms.length] = new Platform(Math.floor(Math.random() * WIDTH - LEFTSIDE.width * 2) + LEFTSIDE.width, i * 500, Math.floor(Math.random() * 1000), (Math.random() <= 0.5) ? "left" : "right"); // testing platform + cool camera
+    window.addEventListener("keydown", function (event) {
+        if (event.key == "z" && gameState == "start") {
+            gameState = "game";
+        }
+    });
+    if (gameState == "game") {
+        // Creates the platforms
+        for (var i = 0; i < numPlatforms; i++) {
+            platforms[platforms.length] = new Platform(Math.floor(Math.random() * WIDTH - SIDEBAR_WIDTH) + SIDEBAR_WIDTH, i * PLATFORM_DISTANCE, Math.floor(Math.random() * 1000), (Math.random() <= 0.5) ? "left" : "right"); // testing platform + cool camera
+        }
+        // Creates the player
+        player = new Player(WIDTH / 2, HEIGHT - 50, PLAYER_SIZE, PLAYER_SIZE, LOG);
+        // Creates the game loop
+        setInterval(update, 1000 / 60); // 60 fps
+        document.addEventListener("keydown", function (event) {
+            if (event.key == "ArrowLeft") {
+                keysPressed.left = true;
+            }
+            else if (event.key == "ArrowRight") {
+                keysPressed.right = true;
+            }
+            else if (event.key == "z") {
+                keysPressed.z = true;
+                player.jump();
+                player.canJump = false;
+            }
+            else if (event.key == "x" || event.key == "c") {
+                keysPressed.x = true;
+                player.dash();
+                player.canDash = false;
+            }
+        });
+        document.addEventListener("keyup", function (event) {
+            if (event.key == "ArrowLeft") {
+                keysPressed.left = false;
+            }
+            else if (event.key == "ArrowRight") {
+                keysPressed.right = false;
+            }
+            else if (event.key == "z") {
+                keysPressed.z = false;
+                player.canJump = true;
+            }
+            else if (event.key == "x" || event.key == "c") {
+                keysPressed.x = false;
+                player.canDash = true;
+            }
+        });
     }
-    // Creates the player
-    player = new Player(WIDTH / 2, HEIGHT - 50, 100, 100, LOG);
-    // Creates the game loop
-    setInterval(update, 16.66); // 60 fps
-    document.addEventListener("keydown", function (event) {
-        if (event.key == "ArrowLeft") {
-            keysPressed.left = true;
-        }
-        else if (event.key == "ArrowRight") {
-            keysPressed.right = true;
-        }
-        else if (event.key == "z") {
-            keysPressed.z = true;
-            player.jump();
-            player.canJump = false;
-        }
-        else if (event.key == "x" || event.key == "c") {
-            keysPressed.x = true;
-            player.dash();
-            player.canDash = false;
-        }
-    });
-    document.addEventListener("keyup", function (event) {
-        if (event.key == "ArrowLeft") {
-            keysPressed.left = false;
-        }
-        else if (event.key == "ArrowRight") {
-            keysPressed.right = false;
-        }
-        else if (event.key == "z") {
-            keysPressed.z = false;
-            player.canJump = true;
-        }
-        else if (event.key == "x" || event.key == "c") {
-            keysPressed.x = false;
-            player.canDash = true;
-        }
-    });
 };
 function update() {
     // Clears the canvas
     ctx.fillStyle = "white";
     ctx.fillRect(0, 0, WIDTH, HEIGHT);
     // Increases camera height
-    cameraHeight += scrollSpeed;
+    if (CAMERA_SCROLL) {
+        cameraHeight += scrollSpeed;
+    }
     // Draws the platforms
     for (var i = 0; i < platforms.length; i++) {
         platforms[i].draw(ctx);
@@ -185,20 +190,45 @@ function update() {
     player.move();
     player.jump();
     // Draws the side images
-    ctx.drawImage(LEFTSIDE, 0, 0, 45, HEIGHT);
-    ctx.drawImage(RIGHTSIDE, WIDTH - RIGHTSIDE.width, 0, 45, HEIGHT);
+    ctx.drawImage(LEFTSIDE, 0, 0, SIDEBAR_WIDTH, HEIGHT);
+    ctx.drawImage(RIGHTSIDE, WIDTH - SIDEBAR_WIDTH, 0, SIDEBAR_WIDTH, HEIGHT);
+    // Score calculation
+    if (Math.floor(player.y / PLATFORM_DISTANCE) > score) {
+        score = Math.floor(player.y / PLATFORM_DISTANCE);
+    }
+    // Score display
+    ctx.fillStyle = "black";
+    ctx.font = "30px Arial";
+    ctx.fillText("Score: " + score, WIDTH - SIDEBAR_WIDTH + 10, 50);
+    // Game over
+    if (cameraHeight - player.y > HEIGHT) {
+        CAMERA_SCROLL = false;
+        ctx.fillStyle = "black";
+        ctx.font = "60px Arial";
+        ctx.fillText("Game Over", WIDTH / 2 - 200, HEIGHT / 2);
+        ctx.fillText("Final Score: " + score, WIDTH / 2 - 200, HEIGHT / 2 + 50);
+        ctx.fillText("Press 'r' to restart", WIDTH / 2 - 200, HEIGHT / 2 + 100);
+        document.addEventListener("keydown", function (event) {
+            if (event.key == "r") {
+                location.reload();
+            }
+        });
+    }
     // Gravity
+    collisionFlag = false;
     platforms.forEach(function (element) {
         if (player.x + player.width > element.x && player.x < element.x + element.width && player.y + player.height > element.y && player.y < element.y + element.height) {
             console.log("collision with " + element);
+            collisionFlag = true;
             if (player.ySpeed > 0) {
                 player.ySpeed = 0;
             }
-            player.onGround = true;
         }
     });
+    if (!collisionFlag) {
+        player.onGround = false;
+    }
     if (!player.onGround) {
-        console.log("flying");
         if (player.ySpeed != 0) { // gravity
             player.ySpeed += GRAVITY;
         }
