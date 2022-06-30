d50bd1801b7a71045393d48a1bbd8c6a5228e762
SkySourced <luka.gn.scott@gmail.com>
2022-07-01 09:39:32 +1200

    updated js

--- src/script.js
+++ src/script.js
@@ -1,45 +1,48 @@
+// Roller
+// a game by Luka Scott
 // *********
 // CONSTANTS
 // *********
-var WIDTH = 800;
-var HEIGHT = 800;
-var GRAVITY = 0.1;
-var SPEED_CAP = 10;
-var PLAYER_SIZE = 100;
-var PLATFORM_SPACING_DIST = 500;
-var SIDEBAR_WIDTH = 45;
-var SPEED_INCREASE = 0.4;
-var SPEED_INCREASE_LENGTH = 100;
+var WIDTH = 800; // canvas width
+var HEIGHT = 800; // canvas height
+var GRAVITY = 0.1; // player y-axis acceleration
+var SPEED_CAP = 10; // speed cap
+var PLAYER_SIZE = 100; // size of player sprite
+var PLATFORM_SPACING_DIST = 500; // distance between platforms
+var SIDEBAR_WIDTH = 45; // width of images on the sides
+var SPEED_INCREASE = 0.4; // how much the camera speed increases
+var SPEED_INCREASE_LENGTH = 100; // how long the speed up is
 // *********
 // IMAGES
 // *********
-var LEFTSIDE = new Image();
+var LEFTSIDE = new Image(); // left sidebar image
 LEFTSIDE.src = "./assets/leftSide.png";
-var RIGHTSIDE = new Image();
+var RIGHTSIDE = new Image(); // right sidebar image
 RIGHTSIDE.src = "./assets/rightSide.png";
-var LOG = new Image();
+var LOG = new Image(); // player image
 LOG.src = "./assets/log.png";
-var PLATFORM_TEXTURE = new Image();
+var PLATFORM_TEXTURE = new Image(); // platform image
 PLATFORM_TEXTURE.src = "./assets/platform.png";
-var SPLASH_SCREEN = new Image();
+var SPLASH_SCREEN = new Image(); // starting image
 SPLASH_SCREEN.src = "./assets/splash screen.png";
 // *********
 // VARIABLES
 // *********
-var ctx;
-var canvas;
-var score = 0;
-var gameState = "start";
+var ctx; // drawing context
+var canvas; // canvas
+var score = 0; // game score
+var level = 1; // game speed level
+var gameState = "start"; // state of the game
 var scrollSpeed = 1; // speed the camera scrolls at
 var cameraHeight = 800; // height the camera starts at
 var platformHeight = 100; // height of platforms
-var numPlatforms = 100;
-var platforms;
-var player;
+var numPlatforms = 100; // number of platforms generated at once
+var platforms; // array of platforms
+var player; // player object
 var collisionFlag; // used to check if player hits any platforms.
 var speedChangeFrameCount; // used to gradually increase the speed when required
-var speedChanging = false;
-var lastFrameCollision = false;
+var speedChanging = false; // is speed increasing
+var lastFrameCollision = false; // was the player colliding last frame
 var keysPressed = {
     left: false,
     right: false,
@@ -51,34 +54,35 @@ var keysPressed = {
 // *********
 var Platform = /** @class */ (function () {
     function Platform(y, side) {
-        this.x = (side == "left") ? 0 : (side == "right") ? WIDTH - PLATFORM_TEXTURE.width : WIDTH / 2 - PLATFORM_TEXTURE.width / 2;
+        this.x = (side == "left") ? 0 : (side == "right") ? WIDTH - PLATFORM_TEXTURE.width : WIDTH / 2 - PLATFORM_TEXTURE.width / 2; // where the platform starts
         this.y = y;
         this.width = PLATFORM_TEXTURE.width;
         this.height = platformHeight;
         this.side = side;
     }
     Platform.prototype.draw = function (ctx) {
-        ctx.fillStyle = "black";
+        //ctx.fillStyle = "black";
+        //ctx.drawRect(this.x, cameraHeight - this.y, this.width, this.height)
         ctx.drawImage(PLATFORM_TEXTURE, this.x, cameraHeight - this.y);
     };
     return Platform;
 }());
 var Player = /** @class */ (function () {
     function Player(x, y, width, height, image) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.image = image;
-        this.moveSpeed = 10;
-        this.jumpHeight = 65;
-        this.dashLength = 170;
-        this.facing = "right";
-        this.dashing = false;
-        this.ySpeed = 0.1;
-        this.onGround = false;
-        this.canDash = false;
-        this.canJump = false;
+        this.x = x; // player position
+        this.y = y; // "
+        this.width = width; // player size
+        this.height = height; // "
+        this.image = image; // image
+        this.moveSpeed = 10; // horizontal move speed
+        this.jumpHeight = 65; // jump ySpeed
+        this.dashLength = 170; // dash length
+        this.facing = "right"; // direction last moved for dashing
+        this.dashing = false; // is player dashing
+        this.ySpeed = 0.1; // y speed
+        this.onGround = false; // is player on ground (can it jump)
+        this.canDash = false; // prevents holding button down and always moving
+        this.canJump = false; // "
     }
     Player.prototype.draw = function (ctx) {
         ctx.drawImage(this.image, this.x, cameraHeight - this.y, this.width, this.height);
@@ -124,22 +128,23 @@ var Player = /** @class */ (function () {
                 this.x += this.moveSpeed;
             }
         }
-        if (this.x < SIDEBAR_WIDTH) { // keep player from going off screen
+        if (this.x < SIDEBAR_WIDTH) { // keep player from going off screen (left)
             this.x = SIDEBAR_WIDTH;
         }
-        if (this.x > WIDTH - this.width - SIDEBAR_WIDTH) {
+        if (this.x > WIDTH - this.width - SIDEBAR_WIDTH) { // keep player from going off screen (right)
             this.x = WIDTH - this.width - SIDEBAR_WIDTH;
         }
     };
     return Player;
 }());
 window.onload = function () {
-    platforms = [];
-    canvas = document.getElementById("mainCanvas");
-    ctx = canvas.getContext("2d");
-    if (gameState == "start") {
+    platforms = []; // inits platforms array
+    canvas = document.getElementById("mainCanvas"); // gets canvas dom object
+    ctx = canvas.getContext("2d"); // gets ctx
+    if (gameState == "start") { // draw splash image
         ctx.drawImage(SPLASH_SCREEN, 0, 0);
     }
+    // initialise event listeners
     window.addEventListener("keydown", keyDownHandler);
     window.addEventListener("keyup", keyUpHandler);
 };
@@ -147,7 +152,7 @@ function gameSetup() {
     if (gameState == "game") {
         // Creates the platforms
         for (var i = 0; i < numPlatforms; i++) {
-            var sideDeterminant = Math.random();
+            var sideDeterminant = Math.random(); // is used to randomly generate position of platforms
             platforms[platforms.length] = new Platform(i * PLATFORM_SPACING_DIST, (sideDeterminant <= 0.33) ? "left" : (sideDeterminant <= 0.66) ? "right" : "centre");
         }
         // Creates the player
@@ -181,16 +186,17 @@ function update() {
         score = Math.floor(player.y / PLATFORM_SPACING_DIST);
         // Attempting to increase scroll speed
         if (score % 5 == 0) {
+            level++;
             speedChanging = true;
             speedChangeFrameCount = 0;
         }
     }
     if (speedChanging) {
-        if (Math.floor(speedChangeFrameCount / 10) % 2 == 0) { // every other 10 frames
-            ctx.fillStyle = "lime";
-            ctx.font = "30px Arial";
-            ctx.fillText("Speed Up!", WIDTH / 2 - 100, HEIGHT / 2);
-        }
+        //if(Math.floor(speedChangeFrameCount / 10) % 2 == 0){ // every other 10 frames
+        ctx.fillStyle = "lime";
+        ctx.font = "60px Arial";
+        ctx.fillText("Speed Up!", WIDTH / 2 - 130, HEIGHT / 2);
+        //}
         scrollSpeed += SPEED_INCREASE / SPEED_INCREASE_LENGTH;
         speedChangeFrameCount++;
         if (speedChangeFrameCount >= SPEED_INCREASE_LENGTH) {
@@ -202,21 +208,27 @@ function update() {
     ctx.font = "30px Arial";
     ctx.fillText("Score: " + score, SIDEBAR_WIDTH + 20, 50);
     // Level display
-    ctx.fillText("Level: " + Math.ceil(score / 5), SIDEBAR_WIDTH + 20, 100);
+    ctx.fillText("Level: " + level, SIDEBAR_WIDTH + 20, 100);
     // Game over
     if (cameraHeight - player.y + player.height > HEIGHT) {
         gameState = "end";
         ctx.fillStyle = "black";
         ctx.font = "60px Arial";
-        ctx.fillText("Game Over", WIDTH / 2 - 200, HEIGHT / 2);
-        ctx.fillText("Final Score: " + score, WIDTH / 2 - 200, HEIGHT / 2 + 50);
-        ctx.fillText("Press 'r' to restart", WIDTH / 2 - 200, HEIGHT / 2 + 100);
+        ctx.fillText("Game Over", WIDTH / 2 - 200, HEIGHT / 2 - 60);
+        ctx.fillText("Final Score: " + score, WIDTH / 2 - 200, HEIGHT / 2);
+        ctx.fillText("Press 'r' to restart", WIDTH / 2 - 200, HEIGHT / 2 + 60);
     }
-    // Gravity 
+    checkCollision();
+    // Debugging
+    //console.log(cameraHeight);
+    //console.log(platforms);
+    //console.log(player.ySpeed)
+}
+function checkCollision() {
+    // Collision 
     collisionFlag = false;
     platforms.forEach(function (platform) {
-        if (player.x + player.width >= platform.x && player.x <= platform.x + platform.width &&
-            player.y + player.height > platform.y && player.y < platform.y + platform.height) {
+        if (player.x + player.width >= platform.x && player.x <= platform.x + platform.width && player.y + player.height > platform.y && player.y < platform.y + platform.height) {
             collisionFlag = true;
             if (player.ySpeed > 0) { // stop player from falling through platforms
                 player.ySpeed = 0;
@@ -227,15 +239,17 @@ function update() {
             else if (platform.y + platform.height > player.y) { // collision from below
                 player.ySpeed = 0.1;
             }
-            console.log("Player X: " + player.x);
-            console.log("Platform height: " + player.width);
-            console.log("Platform X: " + platform.x);
-            console.log("Platform width: " + platform.width);
+            //Debugging collision
+            //console.log("Player X: " + player.x);
+            //console.log("Platform height: " + player.width);
+            //console.log("Platform X: " + platform.x);
+            //console.log("Platform width: " + platform.width);
         }
     });
     if (collisionFlag) {
         lastFrameCollision = true;
     }
+    // Gravity
     if (!collisionFlag) { // sets player as off the ground if not colliding with anything
         if (lastFrameCollision) {
             player.ySpeed += GRAVITY;
@@ -252,16 +266,9 @@ function update() {
             player.ySpeed = -SPEED_CAP;
         }
     }
-    //if (!player.onGround){
-    //}
-    // Debugging
-    //console.log(cameraHeight);
-    //console.log(platforms);
-    //console.log(player.ySpeed)
 }
 // Keyboard input
 function keyDownHandler(event) {
-    var key = event.key;
     if (event.key == "z" && gameState == "start") {
         gameState = "game";
         gameSetup();
@@ -289,7 +296,6 @@ function keyDownHandler(event) {
     }
 }
 function keyUpHandler(event) {
-    var key = event.key;
     if (event.key == "ArrowLeft") {
         keysPressed.left = false;
     }
